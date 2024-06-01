7b0fb13a9720ca5455e3646d9db4355fde6fcc89
Abdulrahman <Abdulrahman@DESKTOP>
2024-06-01 14:12:50 +0300

    Adding Dto that repesent the candidate class with the application db context

---
+++ Candidates.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Candidates.Api.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Candidates.Api.Data
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
