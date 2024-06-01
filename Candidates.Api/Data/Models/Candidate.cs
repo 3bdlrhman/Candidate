6b32f429cb54ac966b29161d0b7abff10db50e0c
Abdulrahman <Abdulrahman@DESKTOP>
2024-06-01 14:12:04 +0300

    Adding Models and Database context

---
+++ Candidates.Api/Data/Models/Candidate.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Candidates.Api.Data.Models
+{
+    public record Candidate
+    {
+        [Key]
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BestCallTime { get; set; }
+        public string LinkedInProfile { get; set; }
+        public string GitHubProfile { get; set; }
+        public string Comment { get; set; }
+    }
+
+}
