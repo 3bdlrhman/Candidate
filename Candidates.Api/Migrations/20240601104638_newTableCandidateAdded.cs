76c4f59891cc5739c17f76e4785df7414e2ce929
Abdulrahman <Abdulrahman@DESKTOP>
2024-06-01 14:13:18 +0300

    Enable migrations

---
+++ Candidates.Api/Migrations/20240601104638_newTableCandidateAdded.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Candidates.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class newTableCandidateAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BestCallTime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LinkedInProfile = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GitHubProfile = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Email);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
